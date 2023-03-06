e7b7dcc3664f8889becea61a251510eada2d6c39
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-03-06 20:24:38 -0300

    feat: Criar botão de deletar e estilizar classe done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,17 @@ const criarTarefa = (evento) => {
    const input = document.querySelector('[data-form-input]');
    const lista = document.querySelector('[data-lista]');
    const valor = input.value;
-   var itens = [];
+  
+   // Criar o elemento 'li' dentro da 'ul'.
+   const tarefa = document.createElement('li');
+   tarefa.classList.add('tarefa');
    
-   itens.push(valor)
-   itens.map(function (valor) {
-      // Criar o elemento 'li' dentro da 'ul'.
-      const tarefa = document.createElement('li');
-      tarefa.classList.add('tarefa');
-      
-      const conteudo = `<p class content>${valor}</p>`;
-      
-      tarefa.innerHTML = conteudo
-      tarefa.appendChild(BotaoConcluir())
-      lista.appendChild(tarefa)
-      console.log(itens)   
-   })
+   const conteudo = `<p class="content">${valor}</p>`;
+   
+   tarefa.innerHTML = conteudo;
+   tarefa.appendChild(BotaoConcluir());
+   tarefa.appendChild(BotaoDeletar());
+   lista.appendChild(tarefa);
    input.value = '';
    input.focus();
 }
@@ -37,10 +33,29 @@ const concluirTarefa = (evento) =>{
   const botaoConcluir = evento.target;
 
   const tarefaConcluida = botaoConcluir.parentElement;
-  tarefaConcluida.classList.toggle('done')
-
+  tarefaConcluida.classList.toggle('done');
+  
 
 }
+const BotaoDeletar = ()=>{
+   const botaoDeletar = document.createElement('button');
+   
+   botaoDeletar.classList.add('botao-delete');
+   botaoDeletar.innerText = 'Deletar'
+
+   botaoDeletar.addEventListener('click', deletarTarefa);
+   return botaoDeletar;
+}
+
+const deletarTarefa = (evento) =>{
+  const botaoDeletar = evento.target;
+
+  const tarefaDeletada = botaoDeletar.parentElement;
+
+  tarefaDeletada.remove()
+  return botaoDeletar;
+}
+
 // Criar evento de click que dispara a Função criarTarefa para adcionar novas tarefas.
 const novaTarefa = document.querySelector('[data-form-buttom]');
 novaTarefa.addEventListener('click', criarTarefa);
