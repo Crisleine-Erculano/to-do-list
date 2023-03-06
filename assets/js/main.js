cb459c62e332e81ff05da4c5a4080afaac36579a
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-03-06 14:37:23 -0300

    feat: Criar botão de concluir tarefa

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,21 +9,40 @@ const criarTarefa = (evento) => {
    itens.map(function (valor) {
       // Criar o elemento 'li' dentro da 'ul'.
       const tarefa = document.createElement('li');
-      tarefa.classList.add('task');
-      const conteudo = `<p class content>${valor}</p>`;
-      tarefa.innerHTML = conteudo
-
-      lista.appendChild(tarefa)
-      console.log(itens)
+      tarefa.classList.add('tarefa');
       
+      const conteudo = `<p class content>${valor}</p>`;
+      
+      tarefa.innerHTML = conteudo
+      tarefa.appendChild(BotaoConcluir())
+      lista.appendChild(tarefa)
+      console.log(itens)   
    })
-
    input.value = '';
    input.focus();
 }
+
+const BotaoConcluir = ()=>{
+const botaoConcluir = document.createElement('button');
+
+botaoConcluir.classList.add('botao-conclui');
+botaoConcluir.innerText = 'Concluir';
+
+botaoConcluir.addEventListener('click', concluirTarefa);
+return botaoConcluir;
+
+
+}
+const concluirTarefa = (evento) =>{
+  const botaoConcluir = evento.target;
+
+  const tarefaConcluida = botaoConcluir.parentElement;
+  tarefaConcluida.classList.toggle('done')
+
+
+}
 // Criar evento de click que dispara a Função criarTarefa para adcionar novas tarefas.
 const novaTarefa = document.querySelector('[data-form-buttom]');
-
 novaTarefa.addEventListener('click', criarTarefa);
 
 
